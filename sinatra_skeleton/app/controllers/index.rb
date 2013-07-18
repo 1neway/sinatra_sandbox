2c778b8eec01d4413648a39349667ff87e8f5b3f
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-18 10:20:33 -0500

    added routes

--- sinatra_skeleton/app/controllers/index.rb
+++ sinatra_skeleton/app/controllers/index.rb
@@ -2,19 +2,25 @@ get '/' do
   # Look in app/views/index.erb
   puts "[LOG] Getting /"
   puts "[LOG] Params: #{params.inspect}"
+  @form_input = params[:user_input]
   erb :index
 end
 
 post '/' do
   puts "[LOG] Posting to /cool_url"
   puts "[LOG] Params: #{params.inspect}"
-  erb :post_cool_url
+    @form_input = params[:user_input]
+
+  redirect to "cool_url/#{@form_input}"
+  # erb :post_cool_url
 end
 
 
-get '/cool_url' do
+get '/cool_url/:user_input' do
   puts "[LOG] Getting /cool_url"
   puts "[LOG] Params: #{params.inspect}"
+  @form_input = params[:user_input]
+
   erb :get_cool_url
 end
 
